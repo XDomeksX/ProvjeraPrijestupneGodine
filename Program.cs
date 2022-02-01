c094e9ad3a8b71ae5af090273f8583afcf5c3295
Domenik Milohanić <90246598+XDomeksX@users.noreply.github.com>
2022-02-01 13:11:48 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProvjeraPrijestupneGodine
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1900; i <= 2100; i++)
+            {
+                do
+                {
+                    i++;
+                    if (i % 4 == 0)
+                    {
+                        Console.WriteLine(i);
+                    }
+                    
+                } while (i <= 2100);
+            }
+            Console.ReadKey();
+        }
+    }
+}
